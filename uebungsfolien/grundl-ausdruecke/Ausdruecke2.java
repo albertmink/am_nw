115c79a035f15e787c2487c8c852b387b06cf179
albert mink <albert.mink@kit.edu>
2018-10-23 10:59:32 +0200

    update code.sty and java code

--- uebungsfolien/grundl-ausdruecke/Ausdruecke2.java
+++ uebungsfolien/grundl-ausdruecke/Ausdruecke2.java
@@ -4,15 +4,15 @@
 public class Ausdruecke2 {
   public static void main (String[] args) {
     System.out.println( "---------boolean----------" );
-    System.out.println( "(k): 1 > 2 = "+(1>2) );
-    System.out.println( "(n): 1 == 2 = "+(1==2) );
-    System.out.println( "(o): (1+1)>1 = "+((1+1)>1 ) );
+    System.out.println( "11: 1 > 2 = "+(1>2) );
+    System.out.println( "14: 1 == 2 = "+(1==2) );
+    System.out.println( "15: (1+1)>1 = "+((1+1)>1 ) );
     System.out.println( "---------modulo-----------" );
-    System.out.println( "(j): 15 %% 2 = "+(15 % 2) );
-    System.out.println( "(e): 14.0%%5 = "+(14.0%5)+" (double)");
+    System.out.println( "10: 15 %% 2 = "+(15 % 2) );
+    System.out.println( "5: 14.0%%5 = "+(14.0%5)+" (double)");
     System.out.println( "-----------ascii----------" );
-    System.out.println( "(l): 'a'+1 = "+('a'+1 ) + " // 98" );
-    System.out.println( "(x): (char)98 = "+((char)98)+" // b" );
-    System.out.println( "(y): 'a'+'b' = "+('a'+'b')+" // 195" );
+    System.out.println( "12: 'a'+1 = "+('a'+1 ) + " // 98" );
+    System.out.println( "(char)98 = "+((char)98)+" // b" );
+    System.out.println( "'a'+'b' = "+('a'+'b')+" // 195" );
   }
 }
