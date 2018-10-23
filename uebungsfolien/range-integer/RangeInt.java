9be75ee8990923129d040f897f2e56de083cba1d
albert mink <albert.mink@kit.edu>
2018-10-23 14:00:55 +0200

    update ex

--- uebungsfolien/range-integer/RangeInt.java
+++ uebungsfolien/range-integer/RangeInt.java
@@ -6,14 +6,9 @@ import java.util.*;
 public class RangeInt {
   public static void main (String[] args)
   {
-    int i = 0;
-    for( ;  i<2147483647; i++)
-    {
-      i += i;
-    }
+    int i = 2147483647;
     System.out.println( i );
     i = i+1;
     System.out.println( i );
-
   }
 }
