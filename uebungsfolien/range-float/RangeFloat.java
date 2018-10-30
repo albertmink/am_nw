4c530faf264248a69b189e327a72083f4fc68425
albert mink <albert.mink@kit.edu>
2018-10-30 13:33:36 +0100

    after excer

--- uebungsfolien/range-float/RangeFloat.java
+++ uebungsfolien/range-float/RangeFloat.java
@@ -2,17 +2,18 @@ import java.util.*;
 /* Minimal example for precision of floats
    Author: Albert Mink, Oct 2018
 */
-
 public class RangeFloat {
   public static void main (String[] args)
   {
-    // float can not represent 0.1 exactly
-    float f = 0.1f;
-    System.out.format( "0.1f = %.11f\n", f);
-    System.out.format( "0.1f+0.1f = %.11f\n", f+f);
+    float f = 1.0000001f;
+    float g = 0.00000001f;
+    System.out.format( "1.0000001f  = %.11f\n", f);
+    System.out.format( "0.00000001f = %.11f\n", g);
+    System.out.format( "1.0000001f+0.00000001f = %.11f\n", f+g);
+    System.out.format( "1.0000001f+...+1.0000001f = %.11f\n", f*10);
     // float can represent 0.5 exactly
-    float g = 0.5f;
-    System.out.format( "0.5f = %.11f\n", g);
-    System.out.format( "0.5f+0.5f = %.11f\n", g+g);
+    float h = 0.5f;
+    System.out.format( "0.5f = %.11f\n", h);
+    System.out.format( "0.5f+0.5f = %.11f\n", h+h);
   }
 }
