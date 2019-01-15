fa1ff6ab963b286e88fab9f1929c43b4ae924e13
albert mink <albert.mink@kit.edu>
2019-01-15 10:20:54 +0100

    rm whitespace endings

--- uebungsfolien/klassen-bild/Bild.java
+++ uebungsfolien/klassen-bild/Bild.java
@@ -1,46 +1,46 @@
-import java.util.*; 
+import java.util.*;
 
-public class Bild 
+public class Bild
 {
-  public static Scanner sc = new Scanner(System.in); 
-  public int size; 
-  public Pixel[][] pixels; 
+  public static Scanner sc = new Scanner(System.in);
+  public int size;
+  public Pixel[][] pixels;
 
-  public Bild() 
+  public Bild()
   {
-    System.out.println("Geben Sie Bildgroesse an"); 
-    size = sc.nextInt(); 
-    pixels = new Pixel[size][size]; 
+    System.out.println("Geben Sie Bildgroesse an");
+    size = sc.nextInt();
+    pixels = new Pixel[size][size];
 
-    for( int x = 0; x < size; x++ ) { 
-      for( int y = 0; y < size; y++ ) { 
+    for( int x = 0; x < size; x++ ) {
+      for( int y = 0; y < size; y++ ) {
         System.out.println("Pixel["+x+"]["+y+"]");
         System.out.println("RGB Wert:");
-        int[] rgb = new int[3]; 
+        int[] rgb = new int[3];
 
-        for( int k = 0; k < rgb.length; k++ ) { 
-          rgb[k] = sc.nextInt(); 
+        for( int k = 0; k < rgb.length; k++ ) {
+          rgb[k] = sc.nextInt();
         }
 
-        pixels[x][y] = new Pixel(x,y,rgb); 
+        pixels[x][y] = new Pixel(x,y,rgb);
       }
     }
   }
 
-  public static void write(Bild bild) 
+  public static void write(Bild bild)
   {
-    for( int x = 0; x < bild.size; x++ ) { 
-      for( int y = 0; y < bild.size; y++ ) { 
-        bild.pixels[x][y].write(); 
+    for( int x = 0; x < bild.size; x++ ) {
+      for( int y = 0; y < bild.size; y++ ) {
+        bild.pixels[x][y].write();
       }
     }
   }
 
   public static void main(String[] args)
   {
-    Bild beltracchi = new Bild(); 
-    Bild mattise = new Bild(); 
-    write(beltracchi); 
-    write(mattise); 
+    Bild beltracchi = new Bild();
+    Bild mattise = new Bild();
+    write(beltracchi);
+    write(mattise);
   }
 }
--- uebungsfolien/klassen-bild/Pixel.java
+++ uebungsfolien/klassen-bild/Pixel.java
@@ -1,18 +1,18 @@
-public class Pixel 
+public class Pixel
 {
-  private int x; 
-  private int y; 
-  private int[] rgb; 
+  private int x;
+  private int y;
+  private int[] rgb;
 
-  public Pixel(int _x, int _y, int[] _rgb) 
+  public Pixel(int _x, int _y, int[] _rgb)
   {
-    x = _x; 
-    y = _y; 
-    rgb = _rgb; 
+    x = _x;
+    y = _y;
+    rgb = _rgb;
   }
 
-  public void write() { 
-    System.out.print( "Pixel["+x+"]["+y+"]: "); 
-    System.out.println(rgb[0]+" "+rgb[1]+" "+rgb[2]); 
+  public void write() {
+    System.out.print( "Pixel["+x+"]["+y+"]: ");
+    System.out.println(rgb[0]+" "+rgb[1]+" "+rgb[2]);
   }
 }
