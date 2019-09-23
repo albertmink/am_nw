d33be4f1260423e49fedf712498baededae4c7b6
albert mink <albert.mink@kit.edu>
2019-09-23 14:21:52 +0200

    faktorial

---
+++ uebungsfolien/schleifen-fakt/SchleifenFakt.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/* Author: Albert Mink, Sep 2019 */
+public class SchleifenFakt {
+  public static void main (String[] args) {
+    int fakultat = 1;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Berechne Fakulaet von ");
+    int n = input.nextInt();
+    for( int i = n; i>0; i-- ) {
+      fakultat *= i;
+    }
+    System.out.println(fakultat);
+    input.close();
+  }
+}
