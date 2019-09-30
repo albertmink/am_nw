68a8f3939b183bef37ee33bde3ac3a133de5fb70
albert mink <albert.mink@kit.edu>
2019-09-30 09:07:55 +0200

    fix formatiing

--- uebungsfolien/felder-berechnungen/Felderb1.java
+++ uebungsfolien/felder-berechnungen/Felderb1.java
@@ -1,11 +1,11 @@
 public class Felderb1 {
   public static void main (String[] args) {  
-    int[] a = new  int[] {1,3,4,2,3};
+    int[] a = new  int[] {1, 3, 4, 2, 3};
     int  e = a[0];
-    for( int i = 1; i < a.length; i++ ) {
+    for( int i = 0; i < a.length; i++ ) {
       if( a[i] > e ) {
         e = a[i];
-	System.out.println(e);
+        System.out.println(e);
       }
     }
     System.out.println("Fertig mit " + e + ".");
