17a7d475d9242ab9fd8d8d8d520816a5d69eaa70
albertmink <38354196+albertmink@users.noreply.github.com>
2019-10-07 08:39:03 +0200

    nw

---
+++ uebungsfolien/anweis-1/Anweis1_snippet_negativ.java
@@ -0,0 +1,13 @@
+if ( n >= 3 ) {
+  if ( n <= 1 ) {
+    System.out.println("A");
+  } else {
+    System.out.println("B");
+  }
+} else {
+  if ( n > 3 ) {
+    System.out.println("C");
+  } else {
+    System.out.println("D");
+  }
+}
