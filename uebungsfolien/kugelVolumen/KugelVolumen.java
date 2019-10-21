6d982b101f444ba9a030e2feae70bc67173b1711
albert mink <albert.mink@kit.edu>
2019-10-21 09:07:58 +0200

    kugel volumen

--- uebungsfolien/kugelVolumen/KugelVolumen.java
+++ uebungsfolien/kugelVolumen/KugelVolumen.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
+import java.util.*;
 /* Berechnung des Kugelvolumens
 Eingabe: r (Kugelradius)
 Ausgabe: v (Kugelvolumen)*/
 
 public class KugelVolumen{
   public static void main (String [] args){
-    double r, v;
     Scanner in = new Scanner(System.in);
     System.out.print("Bitte Kugelradius eingeben: ");
-    r = in.nextDouble();
-    v = 4.0 / 3.0 * Math.PI * r * r * r;
+    double r = in.nextDouble();
+    double v = 4.0 / 3.0 * Math.PI * r * r * r;
     System.out.println("Das Volumen betraegt v = " + v);
   }
 }
+//in.useLocale(Locale.GERMAN);
