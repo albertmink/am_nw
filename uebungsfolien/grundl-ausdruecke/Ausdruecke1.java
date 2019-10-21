7058430c4084eb733d810d4c9c96259f3cdca852
albert mink <albert.mink@kit.edu>
2019-10-21 09:41:33 +0200

    grundl-ausdruecke formatierung

--- uebungsfolien/grundl-ausdruecke/Ausdruecke1.java
+++ uebungsfolien/grundl-ausdruecke/Ausdruecke1.java
@@ -3,15 +3,14 @@
 // grundl-ausdruecke
 public class Ausdruecke1 {
   public static void main (String[] args) {
-    System.out.println( "1: 12.3 + 1 = " + (12.3 + 1) );
-    System.out.println( "2: 0.5*4 = "+(0.5*4)+" (double)" );
-    System.out.println( "7: (int) 1.23 = " + ((int) 1.23) );
-    System.out.println( "---------division-------" );
-    System.out.println( "3: 2.0 / 4.0 = " + (2.0/4.0) );
-    System.out.println( "5: 3 / 6.0 = " + (3 / 6.0) );
-    System.out.println( "9: 1.0 / 4 = " + (1.0 / 4) );
-    System.out.println( "4: (2-1.0) / 5 = "+((2-1.0) / 5) );
-    System.out.println( "12: 1 / 2 = " +(1 / 2) + " (int)" );
-    System.out.println( "8: 2/(int)3.14 = " + (2/(int)3.14) );
+    System.out.println( "12.3 + 1 = " + (12.3 + 1) );
+    System.out.println( "0.5*4 = "+(0.5*4)+" (double)" );
+    System.out.println( " 1 / 2 = " +(1 / 2) + " (int)" );
+    System.out.println( "(int) 1.23 = " + ((int) 1.23) );
+    System.out.println( "2/(int)3.14 = " + (2/(int)3.14) );
+    System.out.println( "2.0 / 4.0 = " + (2.0/4.0) );
+    System.out.println( "3 / 6.0 = " + (3 / 6.0) );
+    System.out.println( "1.0 / 4 = " + (1.0 / 4) );
+    System.out.println( "(2-1.0) / 5 = "+((2-1.0) / 5) );
   }
 }
